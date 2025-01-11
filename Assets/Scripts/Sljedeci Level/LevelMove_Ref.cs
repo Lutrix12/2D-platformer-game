c7ebb6d99ddcc2a157ab4a8347ac71b7d17a6a67
Luka Matošic <lukamatosic.x@gmail.com>
2025-01-11 16:42:17 +0100

    Initial commit

---
+++ Assets/Scripts/Sljedeci Level/LevelMove_Ref.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMove_Ref : MonoBehaviour
+{
+    public int sceneBuildIndex;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "Player")
+        {
+            print("Switching Scene to " + sceneBuildIndex);
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+        }
+    }
+}
